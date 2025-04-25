1488e16d58fe1325f2e5039252c82aa0e132de51
SoHaiNp <139929891+SoHaiNp@users.noreply.github.com>
2025-04-25 15:01:15 -0300

    Mapa adicionado na tela

--- core/src/main/java/io/retronimia/thepromotion/Main.java
+++ core/src/main/java/io/retronimia/thepromotion/Main.java
@@ -1,10 +1,12 @@
 package io.retronimia.thepromotion;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
@@ -12,17 +14,37 @@ public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
 
+    // placeholder...
+    TiledMap tiledMap;
+    OrthogonalTiledMapRenderer tiledMapRenderer;
+    OrthographicCamera orthographicCamera;
+
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+
+        // placeholder...
+        tiledMap = new TmxMapLoader().load("map/placeholderBoard.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 1 / 32f);
+
+        orthographicCamera = new OrthographicCamera();
+        orthographicCamera.setToOrtho(false, 20, 10);
+        orthographicCamera.update();
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
+        // placeholder...
+        orthographicCamera.update();
+
+        tiledMapRenderer.setView(orthographicCamera);
+        tiledMapRenderer.render();
+
         batch.begin();
-        batch.draw(image, 140, 210);
+        // batch.draw(image, 140, 210);
         batch.end();
     }
 
