e168b8d039cf023d40ddefc9bc20147b23faaf57
SoHaiNp <139929891+SoHaiNp@users.noreply.github.com>
2025-05-07 16:36:53 -0300

    Camadas de Mapa e Lerp de Câmera
    Implementado quadrantes para um mapa e interpolação da câmera no momento de trânsição

--- core/src/main/java/io/retronimia/thepromotion/Main.java
+++ core/src/main/java/io/retronimia/thepromotion/Main.java
@@ -1,66 +1,13 @@
 package io.retronimia.thepromotion;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.Game;
+import io.retronimia.thepromotion.screen.DebugScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
-
-    // placeholder...
-    TiledMap tiledMap;
-    OrthogonalTiledMapRenderer tiledMapRenderer;
-    OrthographicCamera orthographicCamera;
-    Viewport viewport;
+public class Main extends Game {
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-
-        // placeholder...
-        tiledMap = new TmxMapLoader().load("map/placeholderBoard.tmx");
-        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 1 / 32f);
-
-        orthographicCamera = new OrthographicCamera();
-        orthographicCamera.setToOrtho(false, 30, 20);
-        orthographicCamera.update();
-
-        viewport = new FitViewport(30, 20, orthographicCamera);
-    }
-
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-
-        // placeholder...
-        orthographicCamera.update();
-
-        tiledMapRenderer.setView(orthographicCamera);
-        tiledMapRenderer.render();
-
-        batch.begin();
-        // batch.draw(image, 140, 210);
-        batch.end();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        viewport.update(width, height);
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
+        this.setScreen(new DebugScreen());
     }
 }
---
+++ core/src/main/java/io/retronimia/thepromotion/map/DebugMap.java
@@ -0,0 +1,82 @@
+package io.retronimia.thepromotion.map;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import space.earlygrey.shapedrawer.ShapeDrawer;
+
+public class DebugMap {
+
+    // 1. Utilitários para Gráficos
+    private ShapeDrawer shapeDrawer;
+
+    // 1. Ativos
+    private Rectangle[] mapLayers;
+    private Vector2[] coordinates;
+
+    // 2. Dados
+    private static final float LAYER_WIDTH = 1440;
+    private static final float LAYER_HEIGHT = 900;
+    private int mapLayerToFocus = 0;
+
+    public DebugMap(SpriteBatch spriteBatch) {
+        // 1. Utilitários
+        shapeDrawer = new ShapeDrawer(spriteBatch);
+
+        coordinates = new Vector2[4];
+        coordinates[0] = new Vector2(-1440, 0);
+        coordinates[1] = new Vector2(0, 0);
+        coordinates[2] = new Vector2(-1440, -900);
+        coordinates[3] = new Vector2(0, -900);
+
+        // 2. Ativos
+        mapLayers = new Rectangle[4];
+        mapLayers[0] = new Rectangle(coordinates[0].x, coordinates[0].y, LAYER_WIDTH, LAYER_HEIGHT);
+        mapLayers[1] = new Rectangle(coordinates[1].x, coordinates[1].y, LAYER_WIDTH, LAYER_HEIGHT);
+        mapLayers[2] = new Rectangle(coordinates[2].x, coordinates[2].y, LAYER_WIDTH, LAYER_HEIGHT);
+        mapLayers[3] = new Rectangle(coordinates[3].x, coordinates[3].y, LAYER_WIDTH, LAYER_HEIGHT);
+    }
+
+    public void drawMapLayers(TextureRegion textureRegion) {
+        shapeDrawer.setTextureRegion(textureRegion);
+
+        for (Rectangle mapLayer : mapLayers) {
+            for (Vector2 coordinate : coordinates) {
+                shapeDrawer.rectangle(mapLayer, Color.BLACK, 10);
+                shapeDrawer.filledCircle(mapLayer.getCenter(coordinate), 50);
+            }
+        }
+    }
+
+    public void debugFocusOnMapLayer(OrthographicCamera orthographicCamera) {
+        if (Gdx.input.isKeyPressed(Input.Keys.NUMPAD_0)) {
+            mapLayerToFocus = 0;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.NUMPAD_1)) {
+            mapLayerToFocus = 1;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.NUMPAD_2)) {
+            mapLayerToFocus = 2;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.NUMPAD_3)) {
+            mapLayerToFocus = 3;
+        }
+
+        // Criar um Vetor que armazena a coordenada do alvo a ser centralizado
+        Vector2 mapLayerCenterCoordinates = mapLayers[mapLayerToFocus].getCenter(coordinates[mapLayerToFocus]);
+        Vector3 mapLayerCenter = new Vector3(mapLayerCenterCoordinates.x, mapLayerCenterCoordinates.y, 0);
+
+        //Criar um Vetor que armazena a posição da câmera
+        Vector3 currentCameraPosition = orthographicCamera.position;
+
+        // Interpolar o Vetor de posição da câmera, usando como alvo o centro que será focado
+        currentCameraPosition.lerp(mapLayerCenter, 0.1f);
+
+        orthographicCamera.position.set(currentCameraPosition);
+        // orthographicCamera.position.set(mapLayers[mapLayerToFocus].getCenter(coordinates[mapLayerToFocus]), 0);
+    }
+
+}
